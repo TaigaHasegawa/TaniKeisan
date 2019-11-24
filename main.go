33c6aabe1daf2f057e327725f2bfdd63977648a5
長谷川太河 <hasegawataiga@i19-taiga.local>
2019-11-24 23:53:24 +0900

    first  commit

--- main.go
+++ main.go
@@ -9,6 +9,40 @@ import (
 	"strconv"
 )
 
+//Post Structure to store db
+type Post struct {
+	Subjects string
+	Tani     string
+	Category string
+}
+
+//POSTS Structure to store db
+var POSTS []Post
+
+//MakeDB for calculating tani
+func MakeDB() []Post {
+	fmt.Println("Start populating tiles db ...")
+	// reading a CSV file
+	file, err := os.Open("posts.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = -1
+	record, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+	var posts []Post
+	for _, item := range record {
+		post := Post{Subjects: item[0], Tani: item[1], Category: item[2]}
+		posts = append(posts, post)
+	}
+	return posts
+}
+
 func main() {
 	fmt.Println("Starting mosaic server ...")
 	mux := http.NewServeMux()
@@ -26,7 +60,6 @@ func main() {
 
 	fmt.Println("Mosaic server started.")
 	server.ListenAndServe()
-
 }
 
 func upload(w http.ResponseWriter, r *http.Request) {
@@ -47,22 +80,26 @@ type results struct {
 	TotalKa          int
 	TotalFuka        int
 	TotalMijyuken    int
-	TotalGoukau      int
+	TotalGoukaku     int
 	TotalSonota      int
 	Result           string
 }
 
 //  Handler function for fan-out and fan-in
 func mosaic(w http.ResponseWriter, r *http.Request) {
-	file, _, err := r.FormFile("uploaded")
-	r.ParseForm()
-	major := r.FormValue("goukaku")
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	defer file.Close()
 	reader := csv.NewReader(file)
 	reader.FieldsPerRecord = -1
+	reader.LazyQuotes = true
 	record, err := reader.ReadAll()
 	if err != nil {
 		panic(err)
 	}
+	major := r.FormValue("goukaku")
 
 	TotalSenmonOne := 0
 	TotalSenmonTwo := 0
@@ -76,23 +113,31 @@ func mosaic(w http.ResponseWriter, r *http.Request) {
 	TotalKa := 0
 	TotalFuka := 0
 	TotalMijyuken := 0
-	TotalGoukau := 0
+	TotalGoukaku := 0
 	TotalSonota := 0
 
 	for _, item := range record {
 		TaniSu, _ := strconv.Atoi(item[6])
 		if item[7] == "優" || item[7] == "良" || item[7] == "可" || item[7] == "優上" || item[7] == "合格" {
-			for _, post := range POSTS {
+			count := 0
+			for i, post := range POSTS {
 				if item[2] == post.Subjects {
 					if post.Category == "専門科目1" {
 						TotalSenmonOne = TotalSenmonOne + TaniSu
+						count = count + 1
 					} else if post.Category == "専門科目2" {
 						TotalSenmonTwo = TotalSenmonTwo + TaniSu
+						count = count + 1
 					} else if post.Category == "専門科目3" {
 						TotalSenmonThree = TotalSenmonThree + TaniSu
+						count = count + 1
 					} else if post.Category == "専門科目4" {
 						TotalSenmonFour = TotalSenmonFour + TaniSu
-					} else {
+						count = count + 1
+					}
+				}
+				if i == len(POSTS)-1 {
+					if count == 0 {
 						TotalSentaku = TotalSentaku + TaniSu
 					}
 				}
@@ -106,7 +151,7 @@ func mosaic(w http.ResponseWriter, r *http.Request) {
 			} else if item[7] == "可" {
 				TotalKa = TotalKa + TaniSu
 			} else if item[7] == "合格" {
-				TotalGoukau = TotalGoukau + TaniSu
+				TotalGoukaku = TotalGoukaku + TaniSu
 			}
 		} else {
 			if item[7] == "不可" {
@@ -118,7 +163,7 @@ func mosaic(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}
-	TotalTani = TotalYujyo + TotalYu + TotalRyou + TotalKa + TotalGoukau
+	TotalTani = TotalYujyo + TotalYu + TotalRyou + TotalKa + TotalGoukaku
 	var result string
 	if TotalTani >= 80 {
 		if major == "経済" {
@@ -126,13 +171,13 @@ func mosaic(w http.ResponseWriter, r *http.Request) {
 				if TotalSenmonTwo >= 18 {
 					result = "卒業見込み"
 				} else {
-					result = "専門科目2不足"
+					result = "卒業不可:　総合単位は足りている。専門科目2(" + strconv.Itoa(18-TotalSenmonOne) + "単位不足)不足。"
 				}
 			} else {
 				if TotalSenmonTwo < 18 {
-					result = "専門科目1,2不足"
+					result = "卒業不可:　総合単位は足りている。専門科目1(" + strconv.Itoa(20-TotalSenmonOne) + "単位不足)" + ",専門科目2(" + strconv.Itoa(18-TotalSenmonTwo) + "単位不足)不足。"
 				} else {
-					result = "専門科目1不足"
+					result = "卒業不可:　総合単位は足りている。専門科目1(" + strconv.Itoa(20-TotalSenmonOne) + "単位不足)不足。"
 				}
 			}
 		} else if major == "経営" {
@@ -140,13 +185,13 @@ func mosaic(w http.ResponseWriter, r *http.Request) {
 				if TotalSenmonThree >= 18 {
 					result = "卒業見込み"
 				} else {
-					result = "専門科目3不足"
+					result = "卒業不可:　総合単位は足りている。専門科目3(" + strconv.Itoa(18-TotalSenmonThree) + "単位不足)不足。"
 				}
 			} else {
 				if TotalSenmonThree < 18 {
-					result = "専門科目1,3不足"
+					result = "卒業不可:　総合単位は足りている。専門科目1(" + strconv.Itoa(20-TotalSenmonOne) + "単位不足)" + ",専門科目3(" + strconv.Itoa(18-TotalSenmonThree) + "単位不足)不足。"
 				} else {
-					result = "専門科目1不足"
+					result = "卒業不可:　総合単位は足りている。専門科目1(" + strconv.Itoa(20-TotalSenmonOne) + "単位不足)不足。"
 				}
 			}
 		} else if major == "金融" {
@@ -154,24 +199,67 @@ func mosaic(w http.ResponseWriter, r *http.Request) {
 				if TotalSenmonFour >= 18 {
 					result = "卒業見込み"
 				} else {
-					result = "専門科目4不足"
+					result = "卒業不可:　総合単位は足りている。専門科目4(" + strconv.Itoa(18-TotalSenmonFour) + "単位不足)不足。"
 				}
 			} else {
 				if TotalSenmonFour < 18 {
-					result = "専門科目1,4不足"
+					result = "卒業不可:　総合単位は足りている。専門科目1(" + strconv.Itoa(18-TotalSenmonOne) + "単位不足)" + ",専門科目4(" + strconv.Itoa(18-TotalSenmonFour) + "単位不足)不足。"
 				} else {
-					result = "専門科目1不足"
+					result = "卒業不可:　総合単位は足りている。専門科目1(" + strconv.Itoa(20-TotalSenmonOne) + "単位不足)不足。"
 				}
 			}
 		}
 	} else {
-		result = "総単位数不足"
+		if major == "経済" {
+			if TotalSenmonOne < 20 {
+				if TotalSenmonTwo < 18 {
+					result = "専門科目1(" + strconv.Itoa(20-TotalSenmonOne) + "単位不足)" + "専門科目2(" + strconv.Itoa(18-TotalSenmonTwo) + "単位不足)不足。"
+				} else {
+					result = "専門科目1(" + strconv.Itoa(20-TotalSenmonOne) + "単位不足)不足。"
+				}
+			} else {
+				if TotalSenmonTwo < 18 {
+					result = "専門科目2(" + strconv.Itoa(18-TotalSenmonTwo) + "単位不足)" + "不足。"
+				} else {
+					result = "ただし専門科目は足りている。"
+				}
+			}
+		} else if major == "経営" {
+			if TotalSenmonOne < 20 {
+				if TotalSenmonThree < 18 {
+					result = "専門科目1(" + strconv.Itoa(20-TotalSenmonOne) + "単位不足)" + "専門科目3(" + strconv.Itoa(18-TotalSenmonThree) + "単位不足)不足。"
+				} else {
+					result = "専門科目1(" + strconv.Itoa(20-TotalSenmonOne) + "単位不足)不足。"
+				}
+			} else {
+				if TotalSenmonThree < 18 {
+					result = "専門科目3(" + strconv.Itoa(18-TotalSenmonThree) + "単位不足)" + "不足。"
+				} else {
+					result = "ただし専門科目は足りている。"
+				}
+			}
+		} else if major == "金融" {
+			if TotalSenmonOne < 20 {
+				if TotalSenmonFour < 18 {
+					result = "専門科目1(" + strconv.Itoa(20-TotalSenmonOne) + "単位不足)" + "専門科目4(" + strconv.Itoa(18-TotalSenmonFour) + "単位不足)不足。"
+				} else {
+					result = "専門科目1(" + strconv.Itoa(20-TotalSenmonOne) + "単位不足)不足。"
+				}
+			} else {
+				if TotalSenmonFour < 18 {
+					result = "専門科目4(" + strconv.Itoa(18-TotalSenmonFour) + "単位不足)" + "不足。"
+				} else {
+					result = "ただし専門科目は足りている。"
+				}
+			}
+		}
+		result = "卒業不可:" + "総合単位(" + strconv.Itoa(80-TotalTani) + "単位不足)不足。" + result
 	}
 
 	ResultLists := results{TotalTani: TotalTani, TotalSenmonOne: TotalSenmonOne, TotalSenmonTwo: TotalSenmonTwo, TotalSenmonThree: TotalSenmonThree, TotalSenmonFour: TotalSenmonFour,
 		TotalSentaku: TotalSentaku, TotalYujyo: TotalYujyo, TotalYu: TotalYu, TotalRyou: TotalRyou, TotalKa: TotalKa,
 		TotalFuka: TotalFuka, TotalMijyuken: TotalMijyuken,
-		TotalGoukau: TotalGoukau, TotalSonota: TotalSonota, Result: result}
+		TotalGoukaku: TotalGoukaku, TotalSonota: TotalSonota, Result: result}
 
 	t, _ := template.ParseFiles("results.html")
 	t.Execute(w, ResultLists)
